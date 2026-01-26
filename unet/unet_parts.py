5548348f9a10bffd02fc80e9aadabc7bcfca1efc
davidsteimel <david.steimel02@gmail.com>
2026-01-26 14:52:59 +0100

    Fix dropout

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -3,16 +3,21 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class DoubleConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, dropout_p=0.0):
         super(DoubleConv, self).__init__()
-        self.double_conv = nn.Sequential(
+
+        layers = [
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
-        )
+        ]
+        if dropout_p > 0.0:
+            layers.append(nn.Dropout(p=dropout_p))
+
+        self.double_conv = nn.Sequential(*layers)
     def forward(self, x):
         return self.double_conv(x)
     
