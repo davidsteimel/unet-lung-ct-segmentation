15883aadf3cb8862f0419c61ea91cd27bf52b9e0
davidsteimel <david.steimel02@gmail.com>
2026-02-03 21:40:13 +0100

    Fix kernel size for testing

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -17,10 +17,10 @@ class DoubleConv(nn.Module):
             dilation = int(3 * (input_res / 256))
 
         layers = [
-            nn.Conv2d(in_channels, out_channels, kernel_size= kernel_size, padding=padding, bias=False),
+            nn.Conv2d(in_channels, out_channels, kernel_size= 3, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=False),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         ]
