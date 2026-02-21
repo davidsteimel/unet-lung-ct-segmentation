Add config.yaml, more precicse profiler, kernel fix, softmax

preprocess.py
@@ -1,118 +1,134 @@
 import os
 import glob
 import random
-import shutil
 import cv2
 import numpy as np
-import utils.config as config
 import json
+import argparse
+import yaml
 
-all_image_files = glob.glob(os.path.join(config.IMAGE_DIR, '*.jpg'))
-all_mask_files = glob.glob(os.path.join(config.MASK_DIR, '*.jpg'))
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_file", required=True, help="Path to config.yaml")
+    args = parser.parse_args()
 
-patient_ids = set()
+    with open(args.config_file, "r") as f:
+        config = yaml.load(f, Loader=yaml.SafeLoader)
 
-for image_path in all_image_files:
-    base_name = os.path.basename(image_path)
-    patient_id = base_name.rsplit('_', 1)[0] 
-    patient_ids.add(patient_id)
+    IMAGE_DIR = config['paths']['image_dir']
+    MASK_DIR = config['paths']['mask_dir']
+    BASE_DIR = config['paths']['base_dir']
+    TARGET_SIZE = tuple(config['hyperparameters']['target_size'])
+    RESOLUTION = str(TARGET_SIZE[1])
+    SEED = config['hyperparameters']['seed']
 
-patient_ids = list(patient_ids)
-random.seed(42)
-random.shuffle(patient_ids)
+    all_image_files = glob.glob(os.path.join(IMAGE_DIR, '*.jpg'))
 
-num_total = len(patient_ids)
-train_split_end = int(num_total * 0.7)
-val_split_end = int(num_total * 0.8)
+    patient_ids = set()
 
-train_ids = set(patient_ids[:train_split_end])
-val_ids = set(patient_ids[train_split_end:val_split_end])
-test_ids = set(patient_ids[val_split_end:])
+    for image_path in all_image_files:
+        base_name = os.path.basename(image_path)
+        patient_id = base_name.rsplit('_', 1)[0] 
+        patient_ids.add(patient_id)
 
-split_info = {
-    "seed": 42,
-    "total_patients": num_total,
-    "train_count": len(train_ids),
-    "val_count": len(val_ids),
-    "test_count": len(test_ids),
-    "train_ids": sorted(list(train_ids)),
-    "val_ids": sorted(list(val_ids)),
-    "test_ids": sorted(list(test_ids))
-}
+    patient_ids = list(patient_ids)
+    random.seed(SEED)
+    random.shuffle(patient_ids)
 
-print(f'Total: {num_total} | Train: {len(train_ids)} | Val: {len(val_ids)} | Test: {len(test_ids)}')
+    num_total = len(patient_ids)
+    train_split_end = int(num_total * 0.7)
+    val_split_end = int(num_total * 0.8)
 
-splits = ['train', 'val', 'test']
-types = ['image', 'mask']
-resolution = str(config.TARGET_SIZE[1])
+    train_ids = set(patient_ids[:train_split_end])
+    val_ids = set(patient_ids[train_split_end:val_split_end])
+    test_ids = set(patient_ids[val_split_end:])
 
-info_file_path = os.path.join(config.BASE_DIR, resolution,"split_info.json")
+    split_info = {
+        "seed": SEED,
+        "total_patients": num_total,
+        "train_count": len(train_ids),
+        "val_count": len(val_ids),
+        "test_count": len(test_ids),
+        "train_ids": sorted(list(train_ids)),
+        "val_ids": sorted(list(val_ids)),
+        "test_ids": sorted(list(test_ids))
+    }
 
-#os.makedirs(os.path.dirname(info_file_path), exist_ok=True)
-with open(info_file_path, "w") as f:
-    json.dump(split_info, f, indent=4)
+    print(f'Total: {num_total} | Train: {len(train_ids)} | Val: {len(val_ids)} | Test: {len(test_ids)}')
 
-for split in splits:
-    for type_ in types:
-        path = os.path.join('data_processed', resolution, split, type_)
-        os.makedirs(path, exist_ok=True)
-        print(f"Created/checked directory: {path}")
+    splits = ['train', 'val', 'test']
+    types = ['image', 'mask']
 
-print(f'Total patients: {num_total}')
-print(f'Training patients: {len(train_ids)}')
-print(f'Validation patients: {len(val_ids)}')
-print(f'Testing patients: {len(test_ids)}')
+    for split in splits:
+        for type_ in types:
+            path = os.path.join(BASE_DIR, RESOLUTION, split, type_)
+            os.makedirs(path, exist_ok=True)
+            print(f"Created/checked directory: {path}")
+
+    info_file_path = os.path.join(BASE_DIR, RESOLUTION,"split_info.json")
+    with open(info_file_path, "w") as f:
+        json.dump(split_info, f, indent=4)
+
+    print(f'Total patients: {num_total}')
+    print(f'Training patients: {len(train_ids)}')
+    print(f'Validation patients: {len(val_ids)}')
+    print(f'Testing patients: {len(test_ids)}')
 
 
-for image_path in all_image_files:
-    base_name = os.path.basename(image_path)
-    patient_id = base_name.rsplit('_', 1)[0] 
+    for image_path in all_image_files:
+        base_name = os.path.basename(image_path)
+        patient_id = base_name.rsplit('_', 1)[0] 
 
-    try:
-        slice_part = base_name.rsplit('_', 1)[1] 
-    except IndexError:
-        print(f"Invalid format skipped: {base_name}")
-        continue
+        try:
+            slice_part = base_name.rsplit('_', 1)[1] 
+        except IndexError:
+            print(f"Invalid format skipped: {base_name}")
+            continue
 
-    mask_base_name = f"{patient_id}_mask_{slice_part}" 
-    mask_path = os.path.join(config.MASK_DIR, mask_base_name)
+        mask_base_name = f"{patient_id}_mask_{slice_part}" 
+        mask_path = os.path.join(MASK_DIR, mask_base_name)
 
-    if not os.path.exists(mask_path):
-        print(f"Warning: Mask not found for {base_name}")
-        continue
+        if not os.path.exists(mask_path):
+            print(f"Warning: Mask not found for {base_name}")
+            continue
 
-    if patient_id in train_ids:
-        split_dir = 'train'
-    elif patient_id in val_ids:
-        split_dir = 'val'
-    elif patient_id in test_ids:
-        split_dir = 'test'
-    else:
-        continue
+        if patient_id in train_ids:
+            split_dir = 'train'
+        elif patient_id in val_ids:
+            split_dir = 'val'
+        elif patient_id in test_ids:
+            split_dir = 'test'
+        else:
+            continue
 
-    # load image as grayscale     
-    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    if img is None: continue
+        # load image as grayscale     
+        img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        if img is None: continue
 
-    if img.shape[:2] == config.TARGET_SIZE:
-        img_resized = img
-    else:
-        img_resized = cv2.resize(img, config.TARGET_SIZE, interpolation=cv2.INTER_CUBIC)
+        if img.shape[:2] == TARGET_SIZE:
+            img_resized = img
+        else:
+            img_resized = cv2.resize(img, TARGET_SIZE, interpolation=cv2.INTER_CUBIC)
 
-    multiclass_mask_color = cv2.imread(mask_path, cv2.IMREAD_COLOR) 
-    
-    if multiclass_mask_color is None: continue
+        # load and process mask
+        multiclass_mask_color = cv2.imread(mask_path, cv2.IMREAD_COLOR) 
+        
+        if multiclass_mask_color is None: continue
 
-    lower_blue = np.array([200, 0, 0])
-    upper_blue = np.array([255, 50, 50])
-    binary_mask = cv2.inRange(multiclass_mask_color, lower_blue, upper_blue)
+        lower_blue = np.array([200, 0, 0])
+        upper_blue = np.array([255, 50, 50])
+        binary_mask = cv2.inRange(multiclass_mask_color, lower_blue, upper_blue)
 
-    if binary_mask.shape[:2] == config.TARGET_SIZE:
-        mask_resized = binary_mask
-    else:
-        mask_resized = cv2.resize(binary_mask, config.TARGET_SIZE, interpolation=cv2.INTER_NEAREST)
+        if binary_mask.shape[:2] == TARGET_SIZE:
+            mask_resized = binary_mask
+        else:
+            mask_resized = cv2.resize(binary_mask, TARGET_SIZE, interpolation=cv2.INTER_NEAREST)
 
-    cv2.imwrite(os.path.join(config.BASE_DIR, resolution, split_dir, 'image', base_name), img_resized)
-    cv2.imwrite(os.path.join(config.BASE_DIR, resolution, split_dir, 'mask', base_name), mask_resized)
+        # Save processed files
+        cv2.imwrite(os.path.join(BASE_DIR, RESOLUTION, split_dir, 'image', base_name), img_resized)
+        cv2.imwrite(os.path.join(BASE_DIR, RESOLUTION, split_dir, 'mask', base_name), mask_resized)
 
-print("Processing completed!")
+    print("Processing completed!")
+
+if __name__ == "__main__":
+    main()

train.py
@@ -1,32 +1,28 @@
 import torch
-import torch.cuda.amp 
+import yaml
 import csv
 import os
 import time
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torch.profiler import ProfilerActivity
 from utils.dice_score import TopKDiceLoss
-from tqdm import tqdm
 import argparse
-import utils.config as config
 from unet.unet_model import UNet
 from utils.data_loading import BasicDataset, get_dataloader
-from utils.utils import (
-    check_accuracy,
-    save_predictions_as_imgs,
-    evaluate
-)
+from utils.utils import evaluate
 from perun import monitor
+torch.set_float32_matmul_precision('high')
 
-def train_fn(loader, model, optimizer, loss_fn, scaler, profiler=None):
+@monitor()
+def train_fn(loader, model, optimizer, loss_fn, device, profiler=None):
     model.train()
-    loop = tqdm(loader)
     running_loss = 0.0
     total_samples = 0
     
-    for batch_idx, data in enumerate(loop):
-        data_img = data['image'].to(config.DEVICE)
-        targets = data['mask'].to(config.DEVICE)
+    for batch in loader:
+        # Load data to device
+        data_img = batch['image'].to(device, non_blocking=True)
+        targets = batch['mask'].to(device, non_blocking=True)
 
         #Forward Pass
         if targets.dim() == 3:
@@ -36,60 +32,72 @@ def train_fn(loader, model, optimizer, loss_fn, scaler, profiler=None):
         
         optimizer.zero_grad()
 
-        with torch.amp.autocast("cuda", dtype=torch.float16): 
+        with torch.amp.autocast("cuda", dtype=torch.bfloat16): 
             predictions = model(data_img)
             loss = loss_fn(predictions, targets)
 
         # Backward Pass
-        scaler.scale(loss).backward()
-        scaler.unscale_(optimizer)
+        loss.backward()
         torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-        scaler.step(optimizer)
-        scaler.update()     
+        optimizer.step()    
 
-        batch_size = data_img.size(0)
-        running_loss += loss.item() * batch_size
-        total_samples += batch_size
+        current_batch_size = data_img.size(0)
+        running_loss += loss.item() * current_batch_size
+        total_samples += current_batch_size
         
         if profiler:
             profiler.step()
 
-        loop.set_postfix(loss=loss.item())
-
     return running_loss / total_samples
 
+@monitor()
 def main():
-    parser = argparse.ArgumentParser(description='UNet Training Script')
-    
-    parser.add_argument('--epochs', '-e', type=int, default=config.NUM_EPOCHS,
-                        help='Num of training epochs')
-    parser.add_argument('--batch-size', '-b', type=int, default=config.BATCH_SIZE,
-                        help='Batch size for training')
-    parser.add_argument('--lr', '--learning-rate', type=float, default=config.LEARNING_RATE,
-                        help='Learning rate for the optimizer')
-    parser.add_argument('--load', '-l', action='store_true', default=config.LOAD_MODEL,
-                        help='Load checkpoint to resume training')
-    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_file", required=True, help="Path to config.yaml")
     args = parser.parse_args()
 
-    config.NUM_EPOCHS = args.epochs
-    config.BATCH_SIZE = args.batch_size
-    config.LEARNING_RATE = args.lr
-    config.LOAD_MODEL = args.load
+    with open(args.config_file, "r") as f:
+        config = yaml.load(f, Loader=yaml.SafeLoader)
 
-    model = UNet(n_channels=config.IN_CHANNELS, n_classes=config.NUM_CLASSES, dropout=config.DROPOUT).to(config.DEVICE)
+    # Hyperparameter
+    learning_rate = config['hyperparameters']['learning_rate']
+    batch_size = config['hyperparameters']['batch_size']
+    num_epoch = config['hyperparameters']['num_epochs']
+    target_size = tuple(config['hyperparameters']['target_size'])
+    kernel_flex = config['hyperparameters']['kernel_flex']
+    
+    # Model
+    channels_in = config['model']['in_channels']
+    num_channels = config['model']['num_classes']
+    
+    # Paths
+    base_dir = config['paths']['base_dir']
+    log_dir = config['paths']['log_dir']
+    res_str = str(target_size[1])
+    train_img_dir = os.path.join(base_dir, res_str, "train", "image")
+    train_mask_dir = os.path.join(base_dir, res_str, "train", "mask")
+    val_img_dir = os.path.join(base_dir, res_str, "val", "image")
+    val_mask_dir = os.path.join(base_dir, res_str, "val", "mask")
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    
+    model = UNet(
+        n_channels=channels_in,
+        n_classes=num_channels, 
+        input_res=target_size[1], 
+        kernel_flex=kernel_flex
+    ).to(device)
     loss_fn = TopKDiceLoss(k=20, smooth=1e-5) 
-    scaler = torch.amp.GradScaler("cuda")
 
     optimizer = optim.AdamW(
         model.parameters(),
-        lr=config.LEARNING_RATE,
+        lr=learning_rate,
         weight_decay=1e-4, 
         betas = (0.9, 0.999),
         eps=1e-4
     )
 
-    scheduler = scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(
         optimizer, 
         mode='max', 
         patience=3,    
@@ -99,104 +107,135 @@ def main():
     )
 
     train_ds = BasicDataset(
-        images_dir=config.TRAIN_IMG_DIR,
-        masks_dir=config.TRAIN_MASK_DIR,
+        images_dir=train_img_dir,
+        masks_dir=train_mask_dir,
         is_train=True
     )
 
     train_loader = get_dataloader(
+        config_dict=config,
         dataset= train_ds,
         mode="train",
-        batch_size=config.BATCH_SIZE
+        batch_size= batch_size
     )
 
     val_ds = BasicDataset(
-        images_dir=config.VAL_IMG_DIR,
-        masks_dir=config.VAL_MASK_DIR,
+        images_dir=val_img_dir,
+        masks_dir=val_mask_dir,
         is_train=False
     )
 
     val_loader = get_dataloader(
+        config_dict=config,
         dataset = val_ds,
         mode="val",
-        batch_size=config.BATCH_SIZE
+        batch_size= batch_size
     )
-
-    print(f"Starting training on {config.DEVICE} for {config.NUM_EPOCHS} epochs \n"
-          f"with a learning rate of {config.LEARNING_RATE} and a batch size of {config.BATCH_SIZE}.")
     
-    os.makedirs(config.LOG_DIR, exist_ok=True)
-    log_file = f"log_{config.TARGET_SIZE[1]}_{config.NUM_EPOCHS}_{config.LEARNING_RATE}_unet.csv"
-    log_path = os.path.join(config.LOG_DIR, log_file)
+    os.makedirs(log_dir, exist_ok=True)
+    log_file = f"log_{res_str}_{num_epoch}_{learning_rate}_unet.csv"
+    log_path = os.path.join(log_dir, log_file)
+
     if not os.path.isfile(log_path):
         with open(log_path, mode="w", newline="") as f:
             writer = csv.writer(f, delimiter=";")
             writer.writerow([
-                    'Epoch',
-                    'Resolution',
-                    'Learning_Rate',
-                    'Batch_Size', 
-                    'Num_Train_Samples',
-                    'Num_Val_Samples',
-                    'Train_Loss', 
-                    'Val_Loss',
-                    'Val_Loss_All',
-                    'Train_Dice',
-                    'Val_Dice', 
-                    'Duration_Sec',
-                    'Train_TP',
-                    'Train_FP',
-                    'Train_TN',
-                    'Train_FN',
-                    'Train_Precision',
-                    'Train_Recall',
-                    'Val_TP',
-                    'Val_FP',
-                    'Val_TN',
-                    'Val_FN',
-                    'Val_Precision',
-                    'Val_Recall'
+                'Num_Params',
+                'Epoch',
+                'Resolution',
+                'Learning_Rate',
+                'Batch_Size', 
+                'Num_Train_Samples',
+                'Num_Val_Samples',
+                'Train_Loss', 
+                'Val_Loss',
+                'Val_Loss_All',
+                'Train_Dice',
+                'Val_Dice', 
+                'Duration_Sec',
+                'Peak_VRAM_GB', 
+                'FLOPs_per_image',
+                'Latency_ms_per_image',
+                'Throughput_img_per_sec',
+                'Train_TP',
+                'Train_FP',
+                'Train_TN',
+                'Train_FN',
+                'Train_Precision',
+                'Train_Recall',
+                'Val_TP',
+                'Val_FP',
+                'Val_TN',
+                'Val_FN',
+                'Val_Precision',
+                'Val_Recall'
                 ])
             
     with torch.profiler.profile(
-        schedule=torch.profiler.schedule(wait=0, warmup=2, active=5, repeat=1),
+        schedule=torch.profiler.schedule(wait=2, warmup=3, active=3, repeat=1),
         on_trace_ready=torch.profiler.tensorboard_trace_handler(config.LOG_DIR),
+        with_flops=True,
         record_shapes=True,
-        with_stack=True
-    ) as prof:
+        with_modules=True,
+        profile_memory=True,
+        activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA]
+    ) as prof:  
         
-        for epoch in range(config.NUM_EPOCHS):
-            print(f"\nEpoch {epoch+1}/{config.NUM_EPOCHS}")
+        for epoch in range(num_epoch):
+            torch.cuda.reset_peak_memory_stats() 
+            torch.cuda.synchronize()
             start_time = time.time()
 
-            train_loss = train_fn(train_loader, model, optimizer, loss_fn, scaler=scaler , profiler=prof if epoch == 0 else None)
+            if epoch == 0:
+                profiler = prof
+            else:
+                profiler = None
 
-            val_metrics = evaluate(val_loader, model, loss_fn, device=config.DEVICE)
-            train_metrics = evaluate(train_loader, model, loss_fn, device=config.DEVICE)
+            train_loss = train_fn(loader=train_loader, model=model, optimizer=optimizer, loss_fn=loss_fn,
+                                   device=device, profiler=profiler)
+
+            val_metrics = evaluate(val_loader, model, loss_fn, device=device)
+            train_metrics = evaluate(train_loader, model, loss_fn, device=device)
 
             scheduler.step(val_metrics['Dice'])
-            duration = time.time() - start_time
-            current_lr = optimizer.param_groups[0]["lr"]
-            train_dice = train_metrics["Dice"]
-            val_loss = val_metrics["Loss"]
-            val_dice = val_metrics["Dice"]
-            val_loss_all = val_metrics["Loss_All"]
+
+            torch.cuda.synchronize()
+            end_time = time.time()
+            duration = end_time - start_time
+
+            peak_vram = torch.cuda.max_memory_allocated(device) / (1024**3)
+            num_samples = len(train_loader.dataset)
+
+            latency_ms = (duration / num_samples) * 1000
+            throughput = num_samples / duration
+
+            if epoch == 0:
+                raw_flops = sum(item.flops for item in prof.key_averages())
+                active_steps = 3 
+                total_flops = raw_flops / (active_steps * batch_size)
+            else:
+                total_flops = 0
 
             with open(log_path, mode="a", newline="") as f:
                 writer = csv.writer(f, delimiter=";")
                 writer.writerow([
+                    sum(p.numel() for p in model.parameters()),
                     int(epoch),
-                    int(config.TARGET_SIZE[1]),
-                    current_lr,
-                    config.BATCH_SIZE,
-                    len(train_loader.dataset),
+                    int(res_str),
+                    optimizer.param_groups[0]["lr"],
+                    batch_size,
+                    num_samples,
                     len(val_loader.dataset),
                     round(float(train_loss), 4),
-                    round(float(val_loss), 4),
-                    round(float(val_loss_all), 4),
-                    round(float(train_dice), 4),
-                    round(float(val_dice), 4),
+                    round(float(val_metrics["Loss"]), 4),
+                    round(float(val_metrics["Loss_All"]), 4),
+                    round(float(train_metrics["Dice"]), 4),
+                    round(float(val_metrics["Dice"]), 4),
                     round(duration, 2),
+                    round(peak_vram, 3),
+                    total_flops,
+                    round(latency_ms, 3),
+                    round(throughput, 2),
                     round(float(train_metrics['TP']), 4),
                     round(float(train_metrics['FP']), 4),
                     round(float(train_metrics['TN']), 4),
@@ -210,10 +249,9 @@ def main():
                     round(float(val_metrics['Precision']), 4),
                     round(float(val_metrics['Recall']), 4)
                 ])
-
-            #save_predictions_as_imgs(
-            #    val_loader, model, folder="saved_images/", device=config.DEVICE, num_examples=8, epoche=epoch
-            #)
+    check_flops = sum(item.flops for item in prof.key_averages())
+    total_flops_per_image = check_flops / (3 * batch_size)
+    print(f"FLOPs per image: {total_flops_per_image}")  
 
 if __name__ == "__main__":
     main()

unet/unet_model.py
@@ -3,19 +3,27 @@ import torch.nn.functional as F
 from .unet_parts import  DoubleConv, Down, Up
 
 class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, dropout=0.5):
+    def __init__(self, n_channels, n_classes, input_res, kernel_flex=False):
         super(UNet, self).__init__()
 
-        self.inc = DoubleConv(n_channels, 64)
-        self.down1 = Down(64, 128, dropout_p= 0.1)
-        self.down2 = Down(128, 256, dropout_p=0.1)
-        self.down3 = Down(256, 512, dropout_p=0.2)
-        self.down4 = Down(512, 1024, dropout_p=0.3)
+        res1 = input_res           
+        res2 = input_res // 2      
+        res3 = input_res // 4      
+        res4 = input_res // 8      
+        res_bottleneck = input_res // 16
+        
+        self.inc = DoubleConv(n_channels, 64, input_res=res1, kernel_flex=kernel_flex)
+        
+        self.down1 = Down(64, 128, input_res=res2, dropout_p= 0.1, kernel_flex=kernel_flex)
+        self.down2 = Down(128, 256, input_res=res3, dropout_p=0.1, kernel_flex=kernel_flex)
+        self.down3 = Down(256, 512, input_res=res4, dropout_p=0.2, kernel_flex=kernel_flex)
+        self.down4 = Down(512, 1024, input_res=res_bottleneck, dropout_p=0.3, kernel_flex=kernel_flex)
 
-        self.up1 = Up(1024,512)
-        self.up2 = Up(512,256)
-        self.up3 = Up(256,128)
-        self.up4 = Up(128,64)
+        self.up1 = Up(1024, 512, input_res=res4, kernel_flex=kernel_flex)
+        self.up2 = Up(512, 256, input_res=res3, kernel_flex=kernel_flex)
+        self.up3 = Up(256, 128, input_res=res2, kernel_flex=kernel_flex)
+        self.up4 = Up(128, 64, input_res=res1, kernel_flex=kernel_flex)
+
         self.outc = nn.Conv2d(64, n_classes, kernel_size=1)
 
 

unet/unet_parts.py
@@ -3,18 +3,17 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class DoubleConv(nn.Module):
-    def __init__(self, in_channels, out_channels, dropout_p=0.0, input_res = 256):
+    def __init__(self, in_channels, out_channels, dropout_p=0.0, input_res = 256, kernel_flex=False):
         super(DoubleConv, self).__init__()
 
-        kernel_size = int(3 * (input_res / 256))
+        if kernel_flex:
+            kernel_size = int(3 * (input_res / 256))
 
-        if kernel_size % 2 == 0: kernel_size += 1 
-        padding = kernel_size // 2
-
-        if input_res == 256:
-            dilation = 1
+            if kernel_size % 2 == 0: kernel_size += 1 
+            padding = kernel_size // 2
         else:
-            dilation = int(3 * (input_res / 256))
+            kernel_size = 3
+            padding = 1
 
         layers = [
             nn.Conv2d(in_channels, out_channels, kernel_size= kernel_size, padding=padding, bias=False),
@@ -32,29 +31,36 @@ class DoubleConv(nn.Module):
         return self.double_conv(x)
     
 class Down(nn.Module):
-    def __init__(self, in_channels, out_channels, dropout_p=0.0):
+    def __init__(self, in_channels, out_channels, dropout_p=0.0, input_res=256, kernel_flex=False):
         super(Down, self).__init__()
-        self.pool = nn.MaxPool2d(2)
-        self.conv = DoubleConv(in_channels, out_channels, dropout_p=dropout_p)
+
+        self.pool = nn.MaxPool2d(stride=2)
+        self.conv = DoubleConv(in_channels=in_channels, out_channels=out_channels, dropout_p=dropout_p, input_res=input_res, kernel_flex=kernel_flex)
 
     def forward(self, x):
+
         x = self.pool(x)
         x = self.conv(x)
+
         return x
     
 class Up(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, input_res=256, kernel_flex=False):
         super().__init__()
+
         self.up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
-        self.conv = DoubleConv(in_channels, out_channels) 
+        self.conv = DoubleConv(in_channels=in_channels, out_channels=out_channels, input_res=input_res, kernel_flex=kernel_flex) 
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
+
         diffY = x2.size()[2] - x1.size()[2]
         diffX = x2.size()[3] - x1.size()[3]
 
         x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
                         diffY // 2, diffY - diffY // 2])
+        
         x = torch.cat([x1, x2], dim=1)
+
         return self.conv(x)
         

utils/data_loading.py
@@ -1,11 +1,8 @@
 import os
 import glob
-import torch
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from pathlib import Path 
-from utils import config
-import torchvision.transforms as transforms 
 import numpy as np
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
@@ -79,12 +76,12 @@ class BasicDataset(Dataset):
             'mask': mask_tensor
         }
     
-def get_dataloader( dataset, mode, batch_size):
+def get_dataloader(config_dict, dataset, mode, batch_size):
         return DataLoader(
             dataset,
             batch_size=batch_size,
             shuffle=(mode == "train"), 
-            num_workers=config.NUM_WORKERS,
-            pin_memory=True,
+            num_workers=config_dict['hyperparameters']['num_workers'],
+            pin_memory=config_dict['hyperparameters']['pin_memory'],
             drop_last=(mode == "train")
         )

utils/dice_score.py
@@ -26,49 +26,53 @@ class TopKDiceLoss(nn.Module):
         super(TopKDiceLoss, self).__init__()
         self.k = k
         self.smooth = smooth
-
+        
+    # logits: (Batch, 2, H, W) -> From Modell
+    # target: (Batch, 1, H, W) -> From DataLoader    
     def forward(self, logits: Tensor, target: Tensor):
-        with torch.amp.autocast("cuda", enabled=False):
-            logits = logits.float()
+        
+        # Calculate probabilities
+        probs = torch.softmax(logits, dim=1)
+        probs = probs[:, 1, ...]  # Assuming class 1 is the foreground class
+        
+        # Ensure the dimensions match
+        if target.dim() == 4 and target.shape[1] == 1:
+            target = target.squeeze(1)
+        elif target.dim() == 3:
+            pass
 
-            # Calculate probabilities since logits are passed which are not normalized
-            probs = torch.sigmoid(logits)
+        assert probs.shape == target.shape, f"Shapes dont match: {probs.shape} vs {target.shape}"
             
-            # Ensure dimensions match
-            if target.dim() == 3:
-                target = target.unsqueeze(1)
-            
-            target = target.float()
-            # Flatten everything to work pixel-wise
-            probs_flat = probs.reshape(probs.shape[0], -1)
-            target_flat = target.reshape(target.shape[0], -1)
+        # Flatten everything to work on a pixel-wise basis
+        probs_flat = probs.reshape(probs.shape[0], -1)
+        target_flat = target.reshape(target.shape[0], -1)
 
-            # Calculate "True Positive Map"
-            tp_map = probs_flat * target_flat
+        # Compute "True Positive Map"
+        tp_map = probs_flat * target_flat
+        
+        mask = torch.ones_like(tp_map)
+        
+        for i in range(probs.shape[0]):
+            foreground_indices = (target_flat[i] == 1)
             
-            mask = torch.ones_like(tp_map)
-            
-            for i in range(probs.shape[0]):
-                foreground_indices = (target_flat[i] == 1)
+            if foreground_indices.sum() > 0:
+                foreground_tp = tp_map[i][foreground_indices]
+
+                k_num = int(foreground_tp.numel() * (self.k / 100.0))
+                k_num = max(1, k_num) 
                 
-                if foreground_indices.sum() > 0:
-                    foreground_tp = tp_map[i][foreground_indices]
+                threshold_val, _ = torch.kthvalue(foreground_tp, k_num)
+                
+                # Create "Ignore Map"
+                pixel_to_ignore = (target_flat[i] == 1) & (tp_map[i] > threshold_val)
+                mask[i][pixel_to_ignore] = 0.0
 
-                    k_num = int(foreground_tp.numel() * (self.k / 100.0))
-                    k_num = max(1, k_num) 
-                    
-                    threshold_val, _ = torch.kthvalue(foreground_tp, k_num)
-                    
-                    # Create "Ignore Map"
-                    pixel_to_ignore = (target_flat[i] == 1) & (tp_map[i] > threshold_val)
-                    mask[i][pixel_to_ignore] = 0.0
+        probs_k = probs_flat * mask
+        target_k = target_flat * mask
 
-            probs_k = probs_flat * mask
-            target_k = target_flat * mask
+        intersection = (probs_k * target_k).sum(dim=1)
+        union = probs_k.sum(dim=1) + target_k.sum(dim=1)
+        
+        dice = (2. * intersection + self.smooth) / (union + self.smooth)
 
-            intersection = (probs_k * target_k).sum(dim=1)
-            union = probs_k.sum(dim=1) + target_k.sum(dim=1)
-            
-            dice = (2. * intersection + self.smooth) / (union + self.smooth)
-
-            return 1 - dice.mean()
+        return 1 - dice.mean()

utils/utils.py
@@ -1,99 +1,12 @@
 import torch
-import os
 from utils.dice_score import dice_coeff
-from torchvision.utils import make_grid
-import torchvision.utils as vutils
 from perun import monitor
 from utils.dice_score import TopKDiceLoss
 
 @torch.no_grad()
-def check_accuracy(loader, model, device="cpu"):
-    num_correct = 0
-    num_pixels = 0
-    dice_score = 0
-    model.eval()
-
-    for batch in loader:
-            x = batch['image'].to(device)
-            true_masks = batch['mask'].to(device)
-
-            if true_masks.dim() == 3:
-                true_masks = true_masks.unsqueeze(1)
-            preds = torch.sigmoid(model(x))
-            preds = (preds > 0.5).float()
-            
-            num_correct += (preds == true_masks).sum()
-            num_pixels += torch.numel(preds)
-            
-            dice_score += dice_coeff(preds, true_masks, reduce_batch_first=False)
-
-    print(f"Accuracy: {num_correct/num_pixels*100:.2f}%")
-    print(f"Dice Score: {dice_score/len(loader)}")
-    
-    model.train()
-    return dice_score / len(loader)
-
-
-@torch.no_grad()
-def save_predictions_as_imgs(
-    loader, model, folder="saved_images/", device="cpu", num_examples=8, epoche = 0):
-    model.eval()
-    os.makedirs(folder, exist_ok=True)
-    images_to_save = []
-    
-    for idx, batch in enumerate(loader):
-            if idx > 0: 
-                break
-            
-            imgs = batch['image'].to(device)
-            true_masks = batch['mask'].to(device)
-
-            if true_masks.dim() == 3:
-                true_masks = true_masks.unsqueeze(1)
-            
-            preds = torch.sigmoid(model(imgs))
-            preds = (preds > 0.5).float()
-
-            for i in range(min(num_examples, imgs.shape[0])):
-                
-                img_single = imgs[i]        # [1, H, W]
-                img_disp = img_single * 0.5 + 0.5
-                img_disp = torch.clamp(img_disp, 0, 1)
-                
-                true_single = true_masks[i] # [1, H, W]
-                pred_single = preds[i]      # [1, H, W]
-
-                # Convert to RGB for better visualization
-                img_rgb = img_disp.repeat(3, 1, 1).float()
-                true_rgb = true_single.repeat(3, 1, 1).float()
-                pred_rgb = pred_single.repeat(3, 1, 1).float()
-
-                overlay = img_rgb * 0.6 
-                
-                tp = (pred_single == 1) & (true_single == 1)
-                fp = (pred_single == 1) & (true_single == 0)
-                fn = (pred_single == 0) & (true_single == 1)
-
-                overlay[1, :, :] += tp.squeeze().float() * 0.4  # Green
-                overlay[0, :, :] += fp.squeeze().float() * 0.8  # Red
-                overlay[2, :, :] += fn.squeeze().float() * 0.8  # Blue
-
-                overlay = torch.clamp(overlay, 0, 1)
-                combined = torch.cat((img_rgb, true_rgb, pred_rgb, overlay), dim=2)
-                
-                images_to_save.append(combined)
-
-    grid = make_grid(images_to_save, nrow=4, padding=10, pad_value=0.5)
-    save_path = os.path.join(folder, "prediction_grid.png")
-    vutils.save_image(grid, f"{folder}/prediction_epoch_{epoche}.png")
-    
-    print(f"Visualization saved: {save_path}")
-    model.train()
-
-@monitor()
-@torch.no_grad()
 def evaluate(loader, model, loss_fn, device, threshold=0.5):
     model.eval()
+
     loss_fn_all = TopKDiceLoss(k=100)
     running_loss = 0.0
     running_loss_all = 0.0
@@ -103,17 +16,14 @@ def evaluate(loader, model, loss_fn, device, threshold=0.5):
     dice_score = 0.0
 
     TP = FP = FN = TN = 0
-    steps = 0
 
     for batch in loader:
         images = batch['image'].to(device)
         true_masks = batch['mask'].to(device)
-        with torch.amp.autocast("cuda", dtype=torch.float16):
+        with torch.amp.autocast("cuda", dtype=torch.bfloat16):
 
-            if true_masks.dim() == 3:
-                true_masks = true_masks.unsqueeze(1).float()
-            else:
-                true_masks = true_masks.float()
+            if true_masks.dim() == 4 and true_masks.shape[1] == 1:
+                true_masks = true_masks.squeeze(1) 
 
             true_masks = (true_masks > 0.5).float()
             logits = model(images) 
@@ -127,7 +37,7 @@ def evaluate(loader, model, loss_fn, device, threshold=0.5):
             total_samples += batch_size
 
             # Probabilities
-            probs = torch.sigmoid(logits)
+            probs = torch.softmax(logits, dim=1)[:, 1]
             preds = (probs > threshold).float()
 
             # Accuracy
@@ -142,8 +52,8 @@ def evaluate(loader, model, loss_fn, device, threshold=0.5):
 
             # Dice
             batch_dice = dice_coeff(
-                preds,
-                true_masks,
+                preds.unsqueeze(1),
+                true_masks.unsqueeze(1),
                 reduce_batch_first=False
             )
             dice_score += batch_dice.item() * batch_size