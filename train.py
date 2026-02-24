Fix precision error, loss with float32

train.py
@@ -34,11 +34,11 @@ def train_fn(loader, model, optimizer, loss_fn, device, profiler=None):
 
         with torch.amp.autocast("cuda", dtype=torch.bfloat16): 
             predictions = model(data_img)
-            loss = loss_fn(predictions, targets)
+            loss = loss_fn(predictions.float(), targets)
 
         # Backward Pass
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=0.5)
         optimizer.step()    
 
         current_batch_size = data_img.size(0)
@@ -92,7 +92,7 @@ def main():
         input_res=target_size[1], 
         kernel_flex=kernel_flex
     ).to(device)
-    loss_fn = TopKDiceLoss(k=20, smooth=1e-5) 
+    loss_fn = TopKDiceLoss(k=50, smooth=1e-5) 
 
     optimizer = optim.AdamW(
         model.parameters(),
@@ -105,8 +105,8 @@ def main():
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(
         optimizer, 
         mode='max', 
-        patience=3,    
-        factor=0.5,   
+        patience=4,    
+        factor=0.8,   
         threshold=1e-3,
         min_lr=1e-6
     )

utils/dice_score.py
@@ -30,6 +30,8 @@ class TopKDiceLoss(nn.Module):
     # logits: (Batch, 2, H, W) -> From Modell
     # target: (Batch, 1, H, W) -> From DataLoader    
     def forward(self, logits: Tensor, target: Tensor):
+        logits = logits.float()
+        target = target.float()
         
         # Calculate probabilities
         probs = torch.softmax(logits, dim=1)
@@ -38,11 +40,7 @@ class TopKDiceLoss(nn.Module):
         # Ensure the dimensions match
         if target.dim() == 4 and target.shape[1] == 1:
             target = target.squeeze(1)
-        elif target.dim() == 3:
-            pass
-
-        assert probs.shape == target.shape, f"Shapes dont match: {probs.shape} vs {target.shape}"
-            
+          
         # Flatten everything to work on a pixel-wise basis
         probs_flat = probs.reshape(probs.shape[0], -1)
         target_flat = target.reshape(target.shape[0], -1)
@@ -50,22 +48,27 @@ class TopKDiceLoss(nn.Module):
         # Compute "True Positive Map"
         tp_map = probs_flat * target_flat
         
-        mask = torch.ones_like(tp_map)
+        # .detch() to seperate the "Ignore Map" from the computational graph,
+        # so it doesn't affect gradients
+        mask = torch.ones_like(tp_map).detach()
         
-        for i in range(probs.shape[0]):
-            foreground_indices = (target_flat[i] == 1)
-            
-            if foreground_indices.sum() > 0:
-                foreground_tp = tp_map[i][foreground_indices]
+        # Determine the threshold for top K% pixels in the foreground class
+        # use torch.no_grad() to ensure this doesn't affect the computational graph
+        with torch.no_grad():
+            for i in range(probs.shape[0]):
+                foreground_indices = (target_flat[i] == 1)
+                
+                if foreground_indices.sum() > 0:
+                    foreground_tp = tp_map[i][foreground_indices]
 
-                k_num = int(foreground_tp.numel() * (self.k / 100.0))
-                k_num = max(1, k_num) 
-                
-                threshold_val, _ = torch.kthvalue(foreground_tp, k_num)
-                
-                # Create "Ignore Map"
-                pixel_to_ignore = (target_flat[i] == 1) & (tp_map[i] > threshold_val)
-                mask[i][pixel_to_ignore] = 0.0
+                    k_num = int(foreground_tp.numel() * (self.k / 100.0))
+                    k_num = max(1, k_num) 
+                    
+                    threshold_val, _ = torch.kthvalue(foreground_tp, k_num)
+                    
+                    # Create "Ignore Map"
+                    pixel_to_ignore = (target_flat[i] == 1) & (tp_map[i] > threshold_val)
+                    mask[i][pixel_to_ignore] = 0.0
 
         probs_k = probs_flat * mask
         target_k = target_flat * mask